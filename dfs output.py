3ef9e736479f27608114e0d6cda9a77bf1d61a5b
RenukuntaAnusha146 <anusharenukunta8@gmail.com>
2026-03-02 15:31:55 +0530

    Add files via upload

---
+++ dfs output.py
@@ -0,0 +1,8 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/AIMLLAB2/Desktop/csm2-078/csm088/dfs.py
+Enter capacities of jugs: 4 3
+Enter goal state: 2 0
+Solution Path:
+[(0, 0), (0, 3), (3, 0), (3, 3), (4, 2), (4, 0), (1, 3), (1, 0), (0, 1), (4, 1), (2, 3), (2, 0)]
