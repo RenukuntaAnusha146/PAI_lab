5acdda44ae49ec392d667df2ac5583153d16bdca
RenukuntaAnusha146 <anusharenukunta8@gmail.com>
2026-03-02 14:35:53 +0530

    Add files via upload

---
+++ exp3 output.py
@@ -0,0 +1,87 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========================================== RESTART: C:/Users/AIMLLAB2/Desktop/csm2-078/exp3.py ===========================================
+Path found: ['A', 'E', 'D', 'G']
+>>> 
+=========================================== RESTART: C:/Users/AIMLLAB2/Desktop/csm2-078/exp4.py ===========================================
+
+Initial configuration
+1-e-2
+6-3-4
+7-5-8
+
+After moving 2 into the empty space
+1-2-e
+6-3-4
+7-5-8
+
+After moving 4 into the empty space
+1-2-4
+6-3-e
+7-5-8
+
+After moving 3 into the empty space
+1-2-4
+6-e-3
+7-5-8
+
+After moving 6 into the empty space
+1-2-4
+e-6-3
+7-5-8
+
+After moving 1 into the empty space
+e-2-4
+1-6-3
+7-5-8
+
+After moving 2 into the empty space
+2-e-4
+1-6-3
+7-5-8
+
+After moving 4 into the empty space
+2-4-e
+1-6-3
+7-5-8
+
+After moving 3 into the empty space
+2-4-3
+1-6-e
+7-5-8
+
+After moving 6 into the empty space
+2-4-3
+1-e-6
+7-5-8
+
+After moving 4 into the empty space
+2-e-3
+1-4-6
+7-5-8
+
+After moving 2 into the empty space
+e-2-3
+1-4-6
+7-5-8
+
+After moving 1 into the empty space
+1-2-3
+e-4-6
+7-5-8
+
+After moving 4 into the empty space
+1-2-3
+4-e-6
+7-5-8
+
+After moving 5 into the empty space
+1-2-3
+4-5-6
+7-e-8
+
+After moving 8 into the empty space. Goal achieved!
+1-2-3
+4-5-6
+7-8-e
