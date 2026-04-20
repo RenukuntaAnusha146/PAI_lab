0515bb6c3c0d2cd4078aa367cea2b8ff55d7c0f6
RenukuntaAnusha146 <anusharenukunta8@gmail.com>
2026-04-20 14:45:33 +0530

    Add files via upload

---
+++ 8&9 casestudy outputs.py
@@ -0,0 +1,14 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======================= RESTART: C:/088/ex8 casestudy.py =======================
+Value Iteration Results:
+State: 0, Value: 39.94
+State: 1, Value: 39.94
+State: 2, Value: 38.13
+>>> 
+==================================================== RESTART: C:/088/exp9 casestudy.py ====================================================
+Policy Iteration Results:
+State: (0, 0), Action: RIGHT, Value: 8.0
+State: (0, 1), Action: DOWN, Value: 10.0
+State: (1, 0), Action: RIGHT, Value: 10.0
---
+++ 8&9 output.py
@@ -0,0 +1,23 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=========================== RESTART: C:/088/exp8&9.py ==========================
+Value Iteration Results:
+State: (0, 0), Value: -2.71
+State: (0, 1), Value: -1.9
+State: (0, 2), Value: -1.0
+State: (1, 0), Value: -1.9
+State: (1, 1), Value: 0.0
+State: (1, 2), Value: 0.0
+State: (2, 0), Value: -1.0
+State: (2, 1), Value: 0.0
+State: (2, 2), Value: 0.0
+
+Policy Iteration Results:
+State: (0, 0), Action: DOWN, Value: -2.71
+State: (0, 1), Action: RIGHT, Value: -1.9
+State: (0, 2), Action: DOWN, Value: -1.0
+State: (1, 0), Action: DOWN, Value: -1.9
+State: (1, 2), Action: DOWN, Value: 0.0
+State: (2, 0), Action: RIGHT, Value: -1.0
+State: (2, 1), Action: RIGHT, Value: 0.0
